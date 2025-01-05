89c27d4cab4c74911084f8f9817ae21b5e20856f
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2025-01-05 22:46:22 +0500

    Reactive Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactive-froms';
+
+  signUp = new FormGroup({
+    name: new FormControl(''),
+    age: new FormControl(''),
+    email: new FormControl('')
+  });
+
+  public myName:any = "";
+  handleSubmit() {
+    console.log(this.signUp.value.name);
+    console.log(this.signUp.value.age);
+    console.log(this.signUp.value.email);
+    this.myName = this.signUp.value.name;
+  }
 }
