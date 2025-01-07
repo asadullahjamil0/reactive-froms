ac0a58252a349b6d9af010a1449c506e603e55ab
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2025-01-07 16:03:05 +0500

    Dropdown list and radio button in Reactive forms.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,15 +11,18 @@ export class AppComponent {
   title = 'reactive-froms';
 
   signUp = new FormGroup({
-    name: new FormControl('', [Validators.required,Validators.minLength(3),Validators.maxLength(20)]),
-    age: new FormControl('', [Validators.required,Validators.min(10),Validators.max(60)]),
-    email: new FormControl('', [Validators.required,Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')])
+    name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
+    age: new FormControl('', [Validators.required, Validators.min(10), Validators.max(60)]),
+    email: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')]),
+    gender: new FormControl('', [Validators.required]),
+    country: new FormControl('', [Validators.required])
   });
 
   public myName: any = "";
   public myAge: any = "";
   public myEmail: any = "";
   handleSubmit() {
+    console.log(this.signUp.value);
     console.log(this.signUp.value.name);
     console.log(this.signUp.value.age);
     console.log(this.signUp.value.email);
