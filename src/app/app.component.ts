2a79a467682696ee8ff4c7432107e4b95b26cb5f
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2025-01-09 23:13:06 +0500

    Multiple Checkboxes in forms.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -9,14 +9,18 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AppComponent {
   title = 'reactive-froms';
+  arrayHobbies: string[] = ['Reading', 'Hiking', 'Travelling'];
 
   signUp = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
     age: new FormControl('', [Validators.required, Validators.min(10), Validators.max(60)]),
     email: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')]),
     gender: new FormControl('', [Validators.required]),
-    country: new FormControl('', [Validators.required])
+    country: new FormControl('', [Validators.required]),
+    accept: new FormControl('', [Validators.requiredTrue]),
+    hobbies: new FormArray([], [Validators.required])
   });
+
 
   public myName: any = "";
   public myAge: any = "";
@@ -33,5 +37,24 @@ export class AppComponent {
 
   get f() {
     return this.signUp.controls;
+  }
+  onChange(e: any) {
+    const checkedValue = e.target.value;
+    const checked = e.target.checked;
+
+    const checkedArray = this.signUp.get('hobbies') as FormArray;
+    if (checked) {
+      checkedArray.push(new FormControl(checkedValue));
+    }
+    else {
+      let i: number = 0;
+      checkedArray.controls.forEach((item) => {
+        if (item.value == checkedValue) {
+          checkedArray.removeAt(i);
+
+        }
+        i++;
+      })
+    }
   }
 }
